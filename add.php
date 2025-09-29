e31ca0b780ac8c7f1012cf8a1575a16a39591c88
Alberto Trujillo <166545167+AlbertoTrujillo-ITB2425@users.noreply.github.com>
2025-09-29 14:51:31 +0000

    29/9/2025

---
+++ add.php
@@ -0,0 +1,15 @@
+*** ADD.PHP ***
+
+<?php
+include 'db.php';
+
+$name  = $_POST['name'];
+$email = $_POST['email'];
+
+$stmt = $conn->prepare("INSERT INTO users (name, email) VALUES (*, ?)");
+$stmt->bind_param("ss", $name, $email);
+$stmt->execute();
+
+header("Location: index.php");
+exit;
+?>
